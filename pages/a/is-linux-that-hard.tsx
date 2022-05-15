2b80d848ce19e1d0367c3eb5fc5b870704902880
lukascobit <lukyn.odehnal333@gmail.com>
2022-05-15 23:11:18 +0200

    some css and linux article

--- pages/a/is-linux-that-hard.tsx
+++ pages/a/is-linux-that-hard.tsx
@@ -5,10 +5,24 @@ function Linux() {
 		<div className="content">
 			<h3>Is Linux that hard?</h3>
 
-			<p>
-				Linux is considered to be "harder" than windows or macos. That
-				isn't really true.
-			</p>
+			<img
+				className="floatRight bigImg"
+				src="https://blog.hboeck.de/uploads/tux.png"
+				alt=""
+			/>
+			<article>
+				<p>
+					Linux is considered to be "harder" than windows or macos.
+					That isn't really true. The "problem" is that people just
+					don't care enought to change their OS. But if you came out
+					of your way just to find this article, then i think you
+					probably should. You will find a lot of linux "propaganda"
+					on the net and some of the points they make are true. Linux
+					for desktop useage isn't for everyone, so is windows, yet
+					like 90% of people use it. The only reason that may hold you
+					back is videogames and shitty proprietary legacy programs.
+				</p>
+			</article>
 		</div>
 	);
 }
