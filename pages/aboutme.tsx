d67f89464be83f83020db274be3cfcdab7915141
lukascobit <lukyn.odehnal333@gmail.com>
2022-09-04 23:01:16 +0200

    basic info

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Aboutme() {
-	return <div className="content">Aboutme</div>;
+	return (
+		<div className="content">
+			<div className="headerText center">About me</div>
+			<article>
+				<p>I'm divear but you may also know me as:</p>
+				<ul>
+					<li>lukascobit</li>
+					<li>lukas_getout</li>
+					<li>divear0</li>
+				</ul>
+			</article>
+		</div>
+	);
 }
 
 export default Aboutme;
