531af738240a55b76937604f806a5c4f45c17562
lukascobit <lukyn.odehnal333@gmail.com>
2022-09-04 22:58:05 +0200

    blogs image

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -10,19 +10,31 @@ function Blogs() {
 	return (
 		<div className="content">
 			<h1 className="headerText center">My blogs</h1>
-			{blogs[+lang].map((e, i) => {
-				return (
-					<div className="blog">
-						<div className="desc">
-							<img src={} alt="" />
-							<a href={`/a/${e.link}`} className="projectLink">
-								{e.name}
-							</a>
-							<h5>{e.desc}</h5>
+			<div className="blogs">
+				{blogs[+lang].map((e, i) => {
+					return (
+						<div className="blog">
+							<div className="desc">
+								<img
+									onClick={() =>
+										(location.href = `/a/${e.link}`)
+									}
+									className="showoffImage blogImg"
+									width={480}
+									height={270}
+									src={e.img}
+									alt="blog image"
+								/>
+								<br />
+								<a href={`/a/${e.link}`} className="blogLink">
+									{e.name}
+								</a>
+								<h5>{e.desc}</h5>
+							</div>
 						</div>
-					</div>
-				);
-			})}
+					);
+				})}
+			</div>
 		</div>
 	);
 }
