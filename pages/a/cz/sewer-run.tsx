e3239e992763978f8c3ecdcc4ee6006e37fe4417
lukascobit <lukyn.odehnal333@gmail.com>
2022-09-07 00:18:58 +0200

    sewer run blog in cz

--- pages/a/cz/sewer-run.tsx
+++ pages/a/cz/sewer-run.tsx
@@ -11,19 +11,19 @@ function Sewer() {
 				alt="sewer run screenshot"
 			/>
 
-			<h1>How I got my profile picture</h1>
+			<h1>Jak jsem získal svou profilovku</h1>
 			<article>
 				<p>
-					Back in the time when I used to make videogames, I made a
-					game called "sewer run". For the main character I draw a
-					neat little guy with a gun/walking stick. A while later I
-					just made it profile my picture
+					Kdysy jsem dělal videohry. Jedna z nich se jmenovala "sewer
+					run", byla to hra o týpkovi, co spadl do stoky a snažil se
+					probojovat ven. Ikonickou hlavní postavu jsem si po pár
+					měsicích dal jako profilovku.
 					<br />
 					<a
 						target="_blank"
 						href="https://lukas-getout.itch.io/sewer-run"
 					>
-						You can play the game here.
+						Hru můžeš hrát tady
 					</a>
 				</p>
 			</article>
