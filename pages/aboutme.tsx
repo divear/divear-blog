b0f91518807cc7ae9fa8a0f58dc04c7a5fcced6f
lukascobit <lukyn.odehnal333@gmail.com>
2022-09-08 22:49:47 +0200

    social media

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import github from "./components/imgs/socmedia/github.png";
+import insta from "./components/imgs/socmedia/insta.png";
+import twitter from "./components/imgs/socmedia/twitter.png";
+import youtube from "./components/imgs/socmedia/youtube.png";
+import Image from "next/image";
+
 function Aboutme() {
 	return (
 		<div className="content">
@@ -12,6 +18,47 @@ function Aboutme() {
 					<li>divear0</li>
 				</ul>
 			</article>
+			<div className="socials">
+				<button
+					onClick={() =>
+						open("https://github.com/lukascobit", "_blank")
+					}
+					className="social"
+				>
+					{" "}
+					<Image width={50} height={50} src={github}></Image>
+				</button>
+				<button
+					onClick={() =>
+						open("https://www.instagram.com/lukas.ode/", "_blank")
+					}
+					className="social"
+				>
+					{" "}
+					<Image width={50} height={50} src={insta}></Image>
+				</button>
+				<button
+					onClick={() =>
+						open("https://twitter.com/divear0", "_blank")
+					}
+					className="social"
+				>
+					{" "}
+					<Image width={50} height={50} src={twitter}></Image>
+				</button>
+				<button
+					onClick={() =>
+						open(
+							"https://www.youtube.com/channel/UCwWXZqqwhdVq50W3vZ4aDSg",
+							"_blank"
+						)
+					}
+					className="social"
+				>
+					{" "}
+					<Image width={50} height={50} src={youtube}></Image>
+				</button>
+			</div>
 		</div>
 	);
 }
