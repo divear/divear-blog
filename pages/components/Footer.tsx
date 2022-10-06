fd5f3783988e4153aa0269e5f0693d4190aef30a
lukascobit <lukyn.odehnal333@gmail.com>
2022-10-06 22:36:54 +0200

    footer links

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import foot from "./imgs/footerImg.png";
+import github from "./imgs/socmedia/github.png";
+import twitter from "./imgs/socmedia/twitter.png";
+import youtube from "./imgs/socmedia/youtube.png";
 
 function Footer() {
+	const socials = [
+		[github, "https://github.com/lukascobit", "lukascobit"],
+		[twitter, "https://twitter.com/divear0", "divear0"],
+		[
+			youtube,
+			"https://www.youtube.com/channel/UCwWXZqqwhdVq50W3vZ4aDSg",
+			"divear",
+		],
+	];
 	return (
 		<div>
 			<Image draggable={false} className="footerImg" src={foot} />
@@ -14,6 +26,23 @@ function Footer() {
 					className="player"
 				>
 					{" "}
+				</div>
+				<div className="smallSocials">
+					{socials.map((s, i) => {
+						return (
+							<button
+								onClick={() => open(s[1].toString(), "_blank")}
+								className="smallSocial"
+								title={s[2].toString()}
+							>
+								<Image
+									width={50}
+									height={50}
+									src={s[0]}
+								></Image>
+							</button>
+						);
+					})}
 				</div>
 				<div className="blackboxNav">
 					<h3>©divear 2022</h3>
