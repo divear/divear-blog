94bf46357e5508263b402d886155fd1d24c1eb02
lukascobit <lukyn.odehnal333@gmail.com>
2022-10-28 21:27:00 +0200

    python backend demo

--- server/server.py
+++ server/server.py
@@ -1,6 +1,32 @@
+import pyttsx3
 import wave
-obj = wave.open("volati.wav", "rb")
-print(obj)
+import pyttsx3
+from pydub import AudioSegment
+
+username = "Martin"
 
 
-# todo: make this somehow work
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+
+
+engine.setProperty('voice', "czech")
+engine.setProperty('rate', 150)
+engine.setProperty('volume', 1.5)
+engine.save_to_file(username, "./name.wav")
+
+engine.runAndWait()
+
+
+
+sound0 = AudioSegment.from_file("./name.wav", format="wav")
+sound1 = AudioSegment.from_file("./volati1.wav", format="wav")
+sound2 = AudioSegment.from_file("./volati2.wav", format="wav")
+
+comb = sound0 + sound1 + sound0 + sound2
+
+file_handle = comb.export("./output.mp3", format="mp3")
+
+
