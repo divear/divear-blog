bdeaa0ad0aae5758adaac53e5d99e0cacc7ab062
lukascobit <lukyn.odehnal333@gmail.com>
2022-11-24 17:49:21 +0100

    gets the b64 but still doesnt send

--- server/server.js
+++ server/server.js
@@ -6,21 +6,33 @@ const port = 4000;
 var gtts = require("node-gtts")("cs");
 var path = require("path");
 
-// maybe just host the file on firebase
+app.use((req, res, next) => {
+	const render = res.render;
+	const send = res.send;
+	res.render = function renderWrapper(...args) {
+		Error.captureStackTrace(this);
+		return render.apply(this, args);
+	};
+	res.send = function sendWrapper(...args) {
+		try {
+			send.apply(this, args);
+		} catch (err) {
+			console.error(
+				`Error in res.send | ${err.code} | ${err.message} | ${res.stack}`
+			);
+		}
+	};
+	next();
+});
 
 app.get("/", function (req, res) {
-	filename = "output.wav";
-	let filePath = path.join(__dirname, filename);
-	let stat = fs.statSync(filePath);
-	res.writeHead(200, {
-		"Content-Type": "audio/mpeg",
-		"Content-Length": stat.size,
-	});
-	res.send({ hello: "test" });
+	out = "output.wav";
+	let outPath = path.join(__dirname, out);
+	let stat = fs.statSync(outPath);
 	const input = req.query.input;
 
-	const filepath = path.join(__dirname, "first.wav");
-	gtts.save(filepath, input, function () {
+	const firstPath = path.join(__dirname, "first.wav");
+	gtts.save(firstPath, input, function () {
 		console.log("save done");
 		ffmpeg("./first.wav") // x
 			.input("./input/volati1.wav") // je prave na drate..
@@ -28,18 +40,17 @@ app.get("/", function (req, res) {
 			.input("./input/volati2.wav") // vola, zvedni to
 			.mergeToFile("./output.wav");
 
-		fs.watch(__dirname, (eventType, filename) => {
-			console.log(`filename provided: ${filename}`);
+		fs.watch(__dirname, (eventType, outPath) => {
+			console.log(`filename provided: ${out}`);
 
 			console.log("merge done");
 			console.log("getting the output");
 			const output = path.join(__dirname, "output.wav");
 			console.log(output);
 
-			var readStream = fs.createReadStream(filePath);
-			console.log(readstream);
-			// We replaced all the event handlers with a simple call to readStream.pipe()
-			readStream.pipe(res);
+			const f = fs.readFileSync(output);
+			contents = f.toString("base64");
+			res.send({ contents });
 
 			return;
 		});
