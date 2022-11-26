b12b9ed9af5d5bb988d0efcf8f603d9acb2616c5
lukascobit <lukyn.odehnal333@gmail.com>
2022-11-26 23:32:28 +0100

    server finally works

--- server/server.js
+++ server/server.js
@@ -3,55 +3,38 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const port = 4000;
-var gtts = require("node-gtts")("cs");
-var path = require("path");
-
-app.use((req, res, next) => {
-	const render = res.render;
-	const send = res.send;
-	res.render = function renderWrapper(...args) {
-		Error.captureStackTrace(this);
-		return render.apply(this, args);
-	};
-	res.send = function sendWrapper(...args) {
-		try {
-			send.apply(this, args);
-		} catch (err) {
-			console.error(
-				`Error in res.send | ${err.code} | ${err.message} | ${res.stack}`
-			);
-		}
-	};
-	next();
-});
+const gtts = require("node-gtts")("cs");
+const path = require("path");
 
 app.get("/", function (req, res) {
-	out = "output.wav";
-	let outPath = path.join(__dirname, out);
-	let stat = fs.statSync(outPath);
 	const input = req.query.input;
 
 	const firstPath = path.join(__dirname, "first.wav");
 	gtts.save(firstPath, input, function () {
 		console.log("save done");
 		ffmpeg("./first.wav") // x
-			.input("./input/volati1.wav") // je prave na drate..
-			.input("./first.wav") // x
-			.input("./input/volati2.wav") // vola, zvedni to
-			.mergeToFile("./output.wav");
+			.input("./input/volati0.wav") // je prave na drate..
+			.mergeToFile("./sec.wav");
+		ffmpeg("sec.wav").audioFilter("atempo=5").output("output.wav").run();
+		// .input("./first.wav") // x
+		// .input("./input/volati2.wav") // vola, zvedni to
 
-		fs.watch(__dirname, (eventType, outPath) => {
-			console.log(`filename provided: ${out}`);
-
-			console.log("merge done");
+		const secPath = path.join(__dirname, "sec.wav");
+		const output = path.join(__dirname, "output.wav");
+		const outfile = fs.readFile("sec.wav", (r) => {
+			console.log(r);
+		});
+		fs.watch(output, () => {
 			console.log("getting the output");
-			const output = path.join(__dirname, "output.wav");
-			console.log(output);
 
-			const f = fs.readFileSync(output);
+			const f = fs.readFileSync(secPath);
 			contents = f.toString("base64");
-			res.send({ contents });
-
+			try {
+				res.write(contents);
+				console.log("sent contents");
+			} catch (error) {
+				console.log(error);
+			}
 			return;
 		});
 	});
