a22b14961a4b266dd96f747c3272da64ee782a7f
lukascobit <lukyn.odehnal333@gmail.com>
2022-12-11 21:46:19 +0100

    analytcics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,25 +3,32 @@ import type { AppProps } from "next/app";
 import * as gtag from "../lib/gtag";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
-import { getAnalytics, app, logEvent } from "../components/firebase/firebase";
+import { getAnalytics, app, logEvent } from "../components/firebase";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter();
+
 	useEffect(() => {
-		// const analytics = getAnalytics(app);
-		// logEvent(analytics, "opened");
-		// const handleRouteChange = (url: string) => {
-		// 	gtag.pageview(url);
-		// };
-		// router.events.on("routeChangeComplete", handleRouteChange);
-		// router.events.on("hashChangeComplete", handleRouteChange);
-		// return () => {
-		// 	router.events.off("routeChangeComplete", handleRouteChange);
-		// 	router.events.off("hashChangeComplete", handleRouteChange);
-		// };
-	}, [router.events]);
+		const analytics = getAnalytics();
+		logEvent(analytics, "notification_received");
+		console.log("analytics");
+	}, []);
+
+	// useEffect(() => { console.log("got analytics");
+
+	// 	logEvent(analytics, "opened");
+	// 	const handleRouteChange = (url: string) => {
+	// 		gtag.pageview(url);
+	// 	};
+	// 	router.events.on("routeChangeComplete", handleRouteChange);
+	// 	router.events.on("hashChangeComplete", handleRouteChange);
+	// 	return () => {
+	// 		router.events.off("routeChangeComplete", handleRouteChange);
+	// 		router.events.off("hashChangeComplete", handleRouteChange);
+	// 	};
+	// }, [router.events]);
 
 	return (
 		<>
--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -5,12 +5,7 @@ import twitter from "../components/imgs/socmedia/twitter.png";
 import youtube from "../components/imgs/socmedia/youtube.png";
 import Image from "next/image";
 import Meta from "../components/Meta";
-import {
-	app,
-	getFirestore,
-	addDoc,
-	collection,
-} from "../components/firebase/firebase";
+import { app, getFirestore, addDoc, collection } from "../components/firebase";
 const db = getFirestore(app);
 
 const socials = [
