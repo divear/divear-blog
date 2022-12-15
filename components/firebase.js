2d178881e8e63f8224de0bef5b51bd62861723ac
lukascobit <lukyn.odehnal333@gmail.com>
2022-12-15 22:28:37 +0100

    d

--- components/firebase.js
+++ components/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { getAnalytics, logEvent } from "firebase/analytics";
 
@@ -9,17 +9,21 @@ const firebaseConfig = {
 
 	authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
 
-	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	projectId: process.env.NEXT_PUBLIC_PROJECTID,
 
 	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
 
 	messagingSenderId: process.env.NEXT_PUBLIC_SENDERID,
+	measurementId: process.env.NEXT_PUBLIC_TRACKID,
 
 	appId: process.env.NEXT_PUBLIC_APPID,
 };
 
 // Initialize Firebase
 
+if (firebase.apps.length === 0) {
+	initializeApp(firebaseConfig);
+}
 const app = initializeApp(firebaseConfig);
 
 export { app, getFirestore, collection, addDoc, getAnalytics, logEvent };
