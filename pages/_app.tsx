5c7cdfcb6e3f8c6611dc97ef161563e47b90df1d
lukascobit <lukyn.odehnal333@gmail.com>
2022-12-17 21:32:26 +0100

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,33 +2,54 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import Script from "next/script";
 import { getAnalytics, app, logEvent } from "../components/firebase.js";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
 	useEffect(() => {
 		const analytics = getAnalytics(app);
 		logEvent(analytics, "opened");
-		console.log("analytics");
-	}, []);
-
-	// useEffect(() => { console.log("got analytics");
-
-	// 	logEvent(analytics, "opened");
-	// 	const handleRouteChange = (url: string) => {
-	// 		gtag.pageview(url);
-	// 	};
-	// 	router.events.on("routeChangeComplete", handleRouteChange);
-	// 	router.events.on("hashChangeComplete", handleRouteChange);
-	// 	return () => {
-	// 		router.events.off("routeChangeComplete", handleRouteChange);
-	// 		router.events.off("hashChangeComplete", handleRouteChange);
-	// 	};
-	// }, [router.events]);
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		router.events.on("hashChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+			router.events.off("hashChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 
 	return (
 		<>
+			<Script
+				async={true}
+				strategy="beforeInteractive"
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3177879883146014"
+			/>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+            });
+        `,
+				}}
+			/>
 			<Nav />
 			<Component {...pageProps} className="content" />
 			<Footer />
