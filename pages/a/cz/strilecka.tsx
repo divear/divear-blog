59e12cbf6079965b485f12bc04d3e797a77b5558
lukascobit <lukyn.odehnal333@gmail.com>
2023-05-29 21:29:44 +0200

    small info fix

--- pages/a/cz/strilecka.tsx
+++ pages/a/cz/strilecka.tsx
@@ -9,7 +9,7 @@ function Game() {
 				Hle, zde máte 3d fps střílečku vytvořenou v Godotu. Možná to i
 				bude fungovat
 			</p>
-			<p>Tady je android verze (nefunguje skákání):</p>
+			<p>Tady je android verze:</p>
 			<button
 				onClick={() =>
 					open(
@@ -24,17 +24,15 @@ function Game() {
 			&quot;nevim&quot;)
 			<br />
 			<div className="">
-				<p>Web verze:</p>
+				<p>
+					Web verze (nedoporučuji - absolutně nefunguje multiplayer):
+				</p>
 				<button
 					onClick={() => open("https://9bhra.netlify.app/", "_blank")}
 				>
 					9bhra.netlify.app
 				</button>
 			</div>
-			<p>
-				Multiplayer někdy funguje někdy ne. Je to docela na random
-				prostě zkoušej idk
-			</p>
 		</div>
 	);
 }
