c02415363fd649dfb9d88ceccfef487e05dd6f10
lukascobit <lukyn.odehnal333@gmail.com>
2023-10-08 20:15:01 +0200

    fix styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Image from "next/image";
 import foot from "./imgs/footerImg.png";
-import github from "./imgs/socmedia/github.png";
-import twitter from "./imgs/socmedia/twitter.png";
-import youtube from "./imgs/socmedia/youtube.png";
+import github from "../components/imgs/socmedia/github.png";
+import twitter from "../components/imgs/socmedia/twitter.png";
+import youtube from "../components/imgs/socmedia/youtube.png";
+import instagram from "../components/imgs/socmedia/insta.png";
+import fiverr from "../components/imgs/socmedia/fiverr.png";
+import upwork from "../components/imgs/socmedia/upwork.png";
 
 function Footer() {
 	const socials = [
 		[github, "https://github.com/divear", "divear"],
 		[twitter, "https://twitter.com/divear0", "divear0"],
+		[instagram, "http://instagram.com/_u/lukas.ode/", "lukas.ode"],
 		[
 			youtube,
 			"https://www.youtube.com/channel/UCwWXZqqwhdVq50W3vZ4aDSg",
 			"divear",
 		],
+		[fiverr, "https://www.fiverr.com/lukasodehnal", "lukasodehnal"],
+		[upwork, "https://www.upwork.com/freelancers/~016ac72e9de97e681f", "Lukas O."],
 	];
 	return (
 		<div>
@@ -37,7 +43,7 @@ function Footer() {
 						return (
 							<button
 								key={i}
-								onClick={() => open(s[1].toString(), "_blank")}
+								onClick={() => open(s[1].toString())}
 								className="smallSocial"
 								title={s[2].toString()}
 							>
