836ff58b0c84c1f4dad71af8b0f03ad747aea273
lukascobit <lukyn.odehnal333@gmail.com>
2023-10-09 19:56:36 +0200

    fix article

--- pages/a/swipechoose.tsx
+++ pages/a/swipechoose.tsx
@@ -22,33 +22,12 @@ function Swipechoose() {
 			/>
 			<br />
 			<p>
-				Likes, those little numbers symbolizing approval, often leave us
-				wondering about their true impact. Honestly, when was the last
-				time you looked at a like count and felt any genuine emotional
-				connection or gained valuable insights? Let&#39;s break free
-				from the like button monotony and introduce a more interactive
-				and entertaining system. Imagine a social media platform where
-				users are presented with two images side by side and asked to
-				choose which one they prefer. It&#39;s like a mini-game that
-				adds an element of fun and engagement. Rather than mindlessly
-				scrolling and liking everything, this approach encourages active
-				participation and thoughtfulness. Let&#39;s move beyond the
-				like-centric mentality and embrace a more playful user
-				interaction. Conclusion: It&#39;s time to question the true
-				significance of likes in our social media experience. By
-				introducing a more interactive and entertaining approach, we can
-				break free from the mundane and inject some fun into our online
-				interactions. Let&#39;s remember that social media is about
-				genuine connections and personal expression, and a little
-				playfulness can go a long way. So, do likes really matter? Maybe
-				it&#39;s time to find a better way to engage and enjoy the
-				digital realm. <br />
+				A site I made that doesn't use likes.
 			</p>
 			<h1>
 				You can find the site here:
 				<a href="https://swipechoose.netlify.app/"> Swipechoose</a>
 			</h1>
-			<p>Rewritten by chatgpt</p>
 		</div>
 	);
 }
