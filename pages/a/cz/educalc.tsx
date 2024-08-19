fd298d634ea8f87f837dc033773102e62c7c8fa1
lukascobit <lukyn.odehnal333@gmail.com>
2024-08-19 21:17:01 +0200

    add cz version of educalc article

---
+++ pages/a/cz/educalc.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Image from "next/image";
+
+function Game() {
+  return (
+    <div className="content">
+      <title>EduCalc</title>
+      <h1>EduCalc - EduPage Calculator</h1>
+      <p>
+        Chtít is vypočítat známku - &quot;Co by mi vycházelo, kdybych dostal za
+        1?&quot; je velice častá potřeba žáků, obzvlášt na konci roku. Přesně
+        kvůli této potřebě jsem naprogramoval kalkulačku průměrů z Vašich
+        EduPage známek.
+      </p>
+      <h1>
+        Program můžete stáhnout z Github releasů, nebo zkompilovat s Cargem:
+        <a
+          href="https://github.com/divear/EduCalc"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          EduCalc
+        </a>
+      </h1>
+      <p>
+        Instrukce k instalaci jsou v{" "}
+        <a
+          target="_blank"
+          href="https://github.com/divear/EduCalc/blob/master/README.md"
+          rel="noreferrer"
+        >
+          README
+        </a>
+      </p>
+      <Image
+        width={400}
+        height={300}
+        src="https://firebasestorage.googleapis.com/v0/b/personalsite-f2369.appspot.com/o/img%2Feducalc1.png?alt=media"
+        alt=""
+      />
+      <Image
+        width={400}
+        height={300}
+        src="https://firebasestorage.googleapis.com/v0/b/personalsite-f2369.appspot.com/o/img%2Feducalc2.png?alt=media"
+        alt=""
+      />
+      <Image
+        width={400}
+        height={300}
+        src="https://firebasestorage.googleapis.com/v0/b/personalsite-f2369.appspot.com/o/img%2Feducalc3.png?alt=media"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Game;
--- pages/a/cz/saliny.tsx
+++ pages/a/cz/saliny.tsx
@@ -2,31 +2,31 @@ import React from "react";
 import Meta from "../../../components/Meta";
 
 function Saliny() {
-	return (
-		<div className="content">
-			<Meta title={"Šaliny"} />
-			<h1>Top nejlepší šaliny v Brně</h1>
-			<iframe
-				width="560"
-				height="315"
-				src="https://www.youtube.com/embed/u9nUzKx49Vg"
-				title="Top nejlepší šaliny v Brně"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-			></iframe>
-			<article>
-				<h2 className="headerText">Proč?</h2>
-				<p>idk</p>
-				<h2 className="headerText">Jak?</h2>
-				<p>
-					Na edit videí používám{" "}
-					<a href="https://www.gimp.org/">Gimp </a>a{" "}
-					<a href="https://kdenlive.org/en/">Kdenlive</a>.
-				</p>
-				<h2 className="headerText">Vážně?</h2>
-				<p>nn</p>
-			</article>
-		</div>
-	);
+  return (
+    <div className="content">
+      <Meta title={"Šaliny"} />
+      <h1>Top nejlepší šaliny v Brně</h1>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/u9nUzKx49Vg"
+        title="Top nejlepší šaliny v Brně"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
+      <article>
+        <h2 className="headerText">Proč?</h2>
+        <p>Svět před vznikem tohoto videa byl jiné místo.</p>
+        <h2 className="headerText">Jak?</h2>
+        <p>
+          Na edit videí používám <a href="https://www.gimp.org/">Gimp </a>a{" "}
+          <a href="https://kdenlive.org/en/">Kdenlive</a>. Při tvorbě videa
+          nebyly použity žádné psychotropní, ani jiné návykové látky.
+        </p>
+        <h2 className="headerText">Proč jsou 3 z nich autobusy?</h2>
+        <p>Pojmy jsou subjektivní a moje trpělivost není nekonečná.</p>
+      </article>
+    </div>
+  );
 }
 
 export default Saliny;
