aa412d2413fa2e022d4fea9fa3981ad80178a51d
divear <lukyn.odehnal333@gmail.com>
2025-07-07 07:43:21 +0200

    loading boilerplate

---
+++ components/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className="loadingPage">
+      <h1>
+       Loading
+      </h1>
+    </div>
+  )
+}
+
+export default Loading
