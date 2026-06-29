e7fd2e8ba97af01866b934c523cfc746c229382c
divear <lukyn.odehnal333@gmail.com>
2026-06-29 23:27:26 +0200

    nvim formatting (ignore)

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,120 +4,120 @@ import pfp from "./imgs/pfp.svg";
 import burger from "./imgs/burgermenu.png";
 import Image from "next/image";
 import Link from "next/link";
-import Loading from "./Loading"
+import Loading from "./Loading";
 
 function Nav() {
-	// false - english
-	// true - czech
-	const [language, setLanguage] = useState(false);
-	const [modal, setModal] = useState(false);
-	const [tree, setTree] = useState(false);
-	const [isPhone, setIsPhone] = useState(false);
+  // false - english
+  // true - czech
+  const [language, setLanguage] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [tree, setTree] = useState(false);
+  const [isPhone, setIsPhone] = useState(false);
 
-	useEffect(() => {
-		setIsPhone(window.innerWidth < 830);
+  useEffect(() => {
+    setIsPhone(window.innerWidth < 830);
 
-		//check if user is czech or slovak & change the site language appropriatly
-		!localStorage.getItem("language") &&
-			localStorage.setItem(
-				"language",
-				navigator.language == "cs" || navigator.language == "sk"
-					? "CZ"
-					: "EN"
-			);
-		localStorage.getItem("language") &&
-			setLanguage(
-				localStorage.getItem("language") == "CZ" ? true : false
-			);
-	}, []);
+    //check if user is czech or slovak & change the site language appropriatly
+    !localStorage.getItem("language") &&
+      localStorage.setItem(
+        "language",
+        navigator.language == "cs" || navigator.language == "sk" ? "CZ" : "EN",
+      );
+    localStorage.getItem("language") &&
+      setLanguage(localStorage.getItem("language") == "CZ" ? true : false);
+  }, []);
 
-	function show() {
-		setModal(!modal);
-	}
-	function change(isCz: boolean) {
-		localStorage.setItem("language", isCz ? "CZ" : "EN");
-		setLanguage(isCz);
-		setModal(false);
+  function show() {
+    setModal(!modal);
+  }
+  function change(isCz: boolean) {
+    localStorage.setItem("language", isCz ? "CZ" : "EN");
+    setLanguage(isCz);
+    setModal(false);
 
-		if (language) {
-			location.href = location.href.replace("/cz/", "/");
-		} else {
-			location.href = location.href.replace("/a/", "/a/cz/");
-		}
-		// location.reload();
-	}
+    if (language) {
+      location.href = location.href.replace("/cz/", "/");
+    } else {
+      location.href = location.href.replace("/a/", "/a/cz/");
+    }
+    // location.reload();
+  }
 
-	return (
-        <div className="nav">
-            <Loading/>
-            <h1
-				className="logoText"
-				onClick={() => (window.location.href = "/")}
-			>
-				<Image
-                    width={50}
-                    height={50}
-                    src={pfp}
-                    alt="i"
-                    style={{
-                        maxWidth: "100%",
-                        height: "auto"
-                    }} />
-				Lukáš Odehnal
-			</h1>
-            <h1 onClick={show} className={isPhone ? "no" : "language"}>
-				{!language ? "🇬🇧" : "🇨🇿"}
-			</h1>
-            <div className={isPhone ? "no" : "redirs"}>
-				<Link href="/projects" legacyBehavior>
-					{!+language ? "Projects" : "Projekty"}
-				</Link>
-				<Link href="/blogs" legacyBehavior>{!+language ? "Blog" : "Články"}</Link>
-				<Link href="/aboutme" legacyBehavior>{!+language ? "About me" : "O mně"}</Link>
-			</div>
-            <div className={modal && !isPhone ? "langModal" : "no"}>
-				<h2 onClick={() => change(true)}>🇨🇿 Česky</h2>
-				<br />
-				<h2 onClick={() => change(false)}>🇬🇧 English</h2>
-			</div>
-            <div
-				onClick={() => setTree(!tree)}
-				className={isPhone ? "burgerMenu" : "no"}
-			>
-				<Image
-                    alt="burgerMenu"
-                    src={burger}
-                    width={50}
-                    height={50}
-                    style={{
-                        maxWidth: "100%",
-                        height: "auto"
-                    }}></Image>
-			</div>
-            <div
-				onClick={() => setTree(false)}
-				className={tree ? "blackOverlay" : "no"}
-			/>
-            <div
-				onClick={() => setTree(false)}
-				className={tree ? "tree" : "no"}
-			>
-				<div className="treeLangModal">
-					<h2 onClick={() => change(true)}>🇨🇿 Česky</h2>
-					<h2 onClick={() => change(false)}>🇬🇧 English</h2>
-				</div>
-				<br />
-				<Link href="/projects" legacyBehavior>
-					{!+language ? "Projects" : "Projekty"}
-				</Link>
-				<br />
-				<Link href="/blogs" legacyBehavior>{!+language ? "Blogs" : "Blogy"}</Link>
-				<br />
-				<Link href="/aboutme" legacyBehavior>{!+language ? "About me" : "O mně"}</Link>
-				<br />
-			</div>
+  return (
+    <div className="nav">
+      <Loading />
+      <h1 className="logoText" onClick={() => (window.location.href = "/")}>
+        <Image
+          width={50}
+          height={50}
+          src={pfp}
+          alt="i"
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+        />
+        Lukáš Odehnal
+      </h1>
+      <h1 onClick={show} className={isPhone ? "no" : "language"}>
+        {!language ? "🇬🇧" : "🇨🇿"}
+      </h1>
+      <div className={isPhone ? "no" : "redirs"}>
+        <Link href="/projects" legacyBehavior>
+          {!+language ? "Projects" : "Projekty"}
+        </Link>
+        <Link href="/blogs" legacyBehavior>
+          {!+language ? "Blog" : "Články"}
+        </Link>
+        <Link href="/aboutme" legacyBehavior>
+          {!+language ? "About me" : "O mně"}
+        </Link>
+      </div>
+      <div className={modal && !isPhone ? "langModal" : "no"}>
+        <h2 onClick={() => change(true)}>🇨🇿 Česky</h2>
+        <br />
+        <h2 onClick={() => change(false)}>🇬🇧 English</h2>
+      </div>
+      <div
+        onClick={() => setTree(!tree)}
+        className={isPhone ? "burgerMenu" : "no"}
+      >
+        <Image
+          alt="burgerMenu"
+          src={burger}
+          width={50}
+          height={50}
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+        ></Image>
+      </div>
+      <div
+        onClick={() => setTree(false)}
+        className={tree ? "blackOverlay" : "no"}
+      />
+      <div onClick={() => setTree(false)} className={tree ? "tree" : "no"}>
+        <div className="treeLangModal">
+          <h2 onClick={() => change(true)}>🇨🇿 Česky</h2>
+          <h2 onClick={() => change(false)}>🇬🇧 English</h2>
         </div>
-    );
+        <br />
+        <Link href="/projects" legacyBehavior>
+          {!+language ? "Projects" : "Projekty"}
+        </Link>
+        <br />
+        <Link href="/blogs" legacyBehavior>
+          {!+language ? "Blogs" : "Blogy"}
+        </Link>
+        <br />
+        <Link href="/aboutme" legacyBehavior>
+          {!+language ? "About me" : "O mně"}
+        </Link>
+        <br />
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -4,51 +4,46 @@ import Meta from "../components/Meta";
 import Image from "next/image";
 
 function Blogs() {
-	const [lang, setLang] = useState(false);
+  const [lang, setLang] = useState(false);
 
-	useEffect(() => {
-		setLang(localStorage.getItem("language") === "EN" ? false : true);
-	}, []);
-	return (
-        <div className="content">
-            <Meta title={+lang ? "Články" : "Blogs"} />
-            <h1 className="headerText center">
-				{+lang ? "Moje články" : "My articles"}
-			</h1>
-            <div className="blogs">
-				{blogs[+lang].map((e, i) => {
-					return (
-                        <div className="blog" key={i}>
-                            <div className="desc">
-								<Image
-                                    onClick={() =>
-										(location.href = `/a/${e.link}`)
-									}
-                                    className="showoffImage blogImg"
-                                    width={1920}
-                                    height={1080}
-                                    src={e.img}
-                                    alt="blogimage"
-                                    style={{
-                                        maxWidth: "100%",
-                                        height: "auto"
-                                    }} />
-								<br />
-								<a
-									rel="noreferrer"
-									href={`/a/${e.link}`}
-									className="blogLink"
-								>
-									{e.name}
-								</a>
-								<p className="blogDesc">{e.desc}</p>
-							</div>
-                        </div>
-                    );
-				})}
-			</div>
-        </div>
-    );
+  useEffect(() => {
+    setLang(localStorage.getItem("language") === "EN" ? false : true);
+  }, []);
+  return (
+    <div className="content">
+      <Meta title={+lang ? "Články" : "Blogs"} />
+      <h1 className="headerText center">
+        {+lang ? "Moje články" : "My articles"}
+      </h1>
+      <div className="blogs">
+        {blogs[+lang].map((e, i) => {
+          return (
+            <div className="blog" key={i}>
+              <div className="desc">
+                <Image
+                  onClick={() => (location.href = `/a/${e.link}`)}
+                  className="showoffImage blogImg"
+                  width={1920}
+                  height={1080}
+                  src={e.img}
+                  alt="blogimage"
+                  style={{
+                    maxWidth: "100%",
+                    height: "auto",
+                  }}
+                />
+                <br />
+                <a rel="noreferrer" href={`/a/${e.link}`} className="blogLink">
+                  {e.name}
+                </a>
+                <p className="blogDesc">{e.desc}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Blogs;
